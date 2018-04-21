824c7bb2a5429d32ec20159657434efb9a725b08
Chris Goller <goller@gmail.com>
2018-04-21 09:29:21 -0500

    cmd(userdata): initial user-data update formatter

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/coreos/coreos-cloudinit/config"
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+var region = flag.String("region", "us-east-1", "aws region")
+var instance = flag.String("instance", "", "aws instance to generate new user-data")
+
+func main() {
+	flag.Parse()
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.Region = *region
+	svc := ec2.New(cfg)
+	input := &ec2.DescribeInstanceAttributeInput{
+		Attribute:  "userData",
+		InstanceId: aws.String(*instance),
+	}
+
+	req := svc.DescribeInstanceAttributeRequest(input)
+	res, err := req.Send()
+	if err != nil {
+		panic(err)
+	}
+
+	if res.UserData == nil || res.UserData.Value == nil {
+		panic("no userdata found")
+	}
+	data, err := base64.StdEncoding.DecodeString(*res.UserData.Value)
+	if err != nil {
+		panic(err)
+	}
+
+	conf, err := config.NewCloudConfig(string(data))
+	if err != nil {
+		panic(err)
+	}
+	updateIdx := -1
+	lockIdx := -1
+	for i := range conf.CoreOS.Units {
+		switch conf.CoreOS.Units[i].Name {
+		case "update-engine.service":
+			if conf.CoreOS.Units[i].Command != "stop" {
+				conf.CoreOS.Units[i].Command = "stop"
+			} else {
+				updateIdx = i
+			}
+		case "locksmithd.service":
+			if conf.CoreOS.Units[i].Command != "stop" {
+				conf.CoreOS.Units[i].Command = "stop"
+			} else {
+				lockIdx = i
+			}
+		}
+	}
+
+	if updateIdx >= 0 && lockIdx >= 0 {
+		os.Exit(1)
+	}
+
+	if updateIdx == -1 {
+		conf.CoreOS.Units = append(conf.CoreOS.Units, config.Unit{
+			Name:    "update-engine.service",
+			Command: "stop",
+		})
+	}
+
+	if lockIdx == -1 {
+		conf.CoreOS.Units = append(conf.CoreOS.Units, config.Unit{
+			Name:    "locksmithd.service",
+			Command: "stop",
+		})
+	}
+
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(string(data), conf.String(), false)
+	fmt.Println(dmp.DiffPrettyText(diffs))
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("if diff looks right, type y (user-data will follow)")
+	text, _ := reader.ReadString('\n')
+	if text == "y\n" {
+		fmt.Printf("New user data:\n\n\n\n\n\n\n%s", conf.String())
+	}
+}
---
+++ vendor/github.com/coreos/coreos-cloudinit/config/unit.go
@@ -0,0 +1,30 @@
+// Copyright 2015 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+type Unit struct {
+	Name    string       `yaml:"name,omitempty"`
+	Mask    bool         `yaml:"mask,omitempty"`
+	Enable  bool         `yaml:"enable,omitempty"`
+	Runtime bool         `yaml:"runtime,omitempty"`
+	Command string       `yaml:"command,omitempty" valid:"^(start|stop|restart|reload|try-restart|reload-or-restart|reload-or-try-restart)$"`
+	Content string       `yaml:"content,omitempty"`
+	DropIns []UnitDropIn `yaml:"drop_ins,omitempty"`
+}
+
+type UnitDropIn struct {
+	Name    string `yaml:"name,omitempty"`
+	Content string `yaml:"content,omitempty"`
+}
